f7df7aaad55249e015291903e868ee1c09352f11
Ando Mickael Ramahenina <ando.mickael25@gmail.com>
2024-05-05 07:37:11 +0300

    firebase upload api

--- api/src/routes/photo.route.js
+++ api/src/routes/photo.route.js
@@ -8,4 +8,21 @@ router.get("/", async (req, res) => {
   res.json(photos);
 });
 
+router.post("/upload", async (req, res) => {
+  const { url, label } = req.body;
+
+  try {
+    const newPhoto = new Photo({
+      link: url,
+      label,
+    });
+
+    const savedPhoto = await newPhoto.save();
+
+    res.json(savedPhoto);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 module.exports = router;

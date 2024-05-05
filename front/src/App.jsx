552f26babc795573862748a1edb254a6c43d6259
Ando Mickael Ramahenina <ando.mickael25@gmail.com>
2024-05-05 10:01:25 +0300

    Revert "implement dashboard"
    This reverts commit 43731c663919dddd7202d6768a06f72d2ac58517.

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UploadPhoto from "./components/UploadPhoto";
-import DashboardUI from "./components/dashboard/Dashboard";
 import LandingUI from "./pages/Landing";
 import Museum from "./pages/Museum";
 import VeteranGalleryPage from "./pages/VeteranGalleryPage/VeteranGalleryPage";
+import Dashboard from "./pages/admin/Dashboard";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 
 const routes = [
   { path: "/", element: <LandingUI /> },
-  { path: "/admin/dashboard", element: <DashboardUI /> },
+  { path: "/admin/dashboard", element: <Dashboard /> },
   { path: "/auth/login", element: <Login /> },
   { path: "/auth/register", element: <Register /> },
   { path: "/veteran-gallery", element: <VeteranGalleryPage /> },

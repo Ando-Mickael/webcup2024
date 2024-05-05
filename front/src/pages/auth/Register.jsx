254817b6b3d52fe54674fb85a155c3e79cf1ef7b
Ando Mickael Ramahenina <ando.mickael25@gmail.com>
2024-05-05 09:15:50 +0300

    integrate register

--- front/src/pages/auth/Register.jsx
+++ front/src/pages/auth/Register.jsx
@@ -1,22 +1,66 @@
+import "../../assets/css/Register.css";
+import fanou from "../../assets/wave.gif";
+import { apiUrl } from "../../libs/api";
+
 export default function Register() {
-  async function handleSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
 
     const data = {
       email: event.target.email.value,
       password: event.target.password.value,
+      nickname: event.target.nickname.value,
     };
 
-    console.log(data);
+    fetch(`${apiUrl}/auth/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="email" name="email" />
-        <input type="password" name="password" />
-        <button type="submit">Register</button>
-      </form>
-    </div>
+    <>
+      <section>
+        <div className="section-background"></div>{" "}
+        {/* Ajoutez une div pour le fond GIF */}
+        <div className="section-overlay"></div>{" "}
+        {/* Ajoutez une div pour la superposition */}
+        <div className="login-box">
+          <form onSubmit={handleSubmit}>
+            <h2> Register</h2>
+            <div className="input-box">
+              <span className="icon">
+                <i className="fas fa-envelope"></i>
+                <input type="text" name="email" required />
+                <label>Email</label>
+              </span>
+            </div>
+            <div className="input-box">
+              <span className="icon">
+                <i className="fas fa-user"></i>
+                <input type="text" name="nickname" required />
+                <label>Nickname</label>
+              </span>
+            </div>
+            <div className="input-box">
+              <span className="icon">
+                <i className="fas fa-unlock"></i>
+                <input type="password" name="password" required />
+                <label>Password</label>
+              </span>
+            </div>
+            <button type="submit">Sign in</button>
+          </form>
+        </div>
+      </section>
+      <img src={fanou} alt="" className="fanou" />
+    </>
   );
 }
